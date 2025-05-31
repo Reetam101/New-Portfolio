62b5f14e3da91490822b3266050b5331e915c46a
Reetam101 <reetamraj2@gmail.com>
2025-05-31 11:26:29 +0530

    finished the hero section, working on the featured projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,18 +1,35 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "Fortmindz Private Ltd.",
+    year: "2025",
+    title: "Happs",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      {
+        title:
+          "Event Discovery & Booking: Built a robust API to fetch event listings, handle ticket purchases, and manage user bookings.",
+      },
+      {
+        title:
+          "Real-time Push Notifications: Implementing real-time updates for booking confirmations, reminders, and event updates using WebSockets & Firebase Cloud Messaging (FCM).",
+      },
+      {
+        title:
+          "Scalable API Architecture: Designing a high-performance backend with Node.js, Express, and MongoDB, optimized for concurrent users.",
+      },
+      {
+        title:
+          "Secure Authentication: Implementing role-based authentication for users, event organizers, and admins.",
+      },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "https://happslive.com/",
     image: darkSaasLandingPage,
   },
   {
@@ -42,5 +59,62 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4">
+          See how I transformed concepts into engaging software experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl overflow-hidden z-0 after:z-10 relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8"
+            >
+              <div
+                className="absolute inset-0"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                <span>{project.company}</span>
+                <span>&bull;</span>
+                <span>{project.year}</span>
+              </div>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
+              <ul className="flex flex-col gap-4 mt-4">
+                {project.results.map((result) => (
+                  <li className="flex gap-2 text-sm text-white/50">
+                    <CheckCircleIcon className="size-5" />
+                    <span>{result.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>Visit Live Site</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
+              </a>
+              <Image
+                src={project.image}
+                alt={project.title}
+                className="mt-8 -mb-4"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
